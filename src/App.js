701a211ab591f89447f924bdf257548ec8b787c1
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2022-08-12 13:34:12 -0300

    alteração no arquivo app.js foi feita

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
@@ -12,7 +12,7 @@ import Footer from './components/layout/Footer'
 
 function App() {
   return (
-    <Router>
+    <Router basename='/'>
       <Navbar/>
       <Container customClass="min-height">
         <Routes>
